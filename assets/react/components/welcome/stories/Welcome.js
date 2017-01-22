87068974397d2988c9e0e0a2faa5da592ee6e5c2
Jonny Levy <iamjonnylevy@gmail.com>
2017-01-22 13:11:47 +0000

    chore(welcome): update story
    Signed-off-by: Jonny Levy <iamjonnylevy@gmail.com>

--- assets/react/components/welcome/stories/Welcome.js
+++ assets/react/components/welcome/stories/Welcome.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+
 import Welcome from '../Welcome';
+import { USER_TURN_GAMES } from '../../../mocks/userTurnGames';
 
 storiesOf('Welcome', module)
   .add('regular state', () => (
-    <Welcome startGame={action('Start Game')} />
+    <Welcome startGame={action('Start Game')} usersTurnGames={USER_TURN_GAMES} />
   ));
