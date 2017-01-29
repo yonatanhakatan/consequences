7e709d892d7e3ace5e4f63cfd447f08c1995483c
Jonny Levy <iamjonnylevy@gmail.com>
2017-01-29 17:47:11 +0000

    storybook(game): update stories
    Signed-off-by: Jonny Levy <iamjonnylevy@gmail.com>

--- assets/react/components/game/stories/Game.js
+++ assets/react/components/game/stories/Game.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf, action } from '@kadira/storybook';
 import Game from '../Game';
+import { INITIAL_CATEGORIES, FINAL_CATEGORIES } from '../../../mocks/categories';
 
 storiesOf('Game', module)
-  .add('regular state', () => (
+  .add('User\'s Turn', () => (
+    <Game gameState="userTurn" categories={INITIAL_CATEGORIES} onEntrySubmit={action('Entry Submit')} />
+  ))
+  .add('Turn just played', () => (
+    <Game gameState="turnJustPlayed" categories={INITIAL_CATEGORIES} onEntrySubmit={action('Entry Submit')} />
+  ))
+  .add('Turn Ended', () => (
+    <Game gameState="turnEnded" />
+  ))
+  .add('Not User\'s turn', () => (
+    <Game gameState="notUserTurn" />
+  ))
+  .add('Game Finished', () => (
+    <Game gameState="finished" categories={FINAL_CATEGORIES} onEntrySubmit={action('Entry Submit')} />
+  ))
+  .add('Initialising', () => (
     <Game />
   ));
---
+++ assets/react/mocks/categories.js
@@ -0,0 +1,46 @@
+/* eslint-disable max-len */
+export const INITIAL_CATEGORIES = [
+  {
+    label: 'Enter a man\'s name',
+  },
+];
+
+export const FINAL_CATEGORIES = [
+  {
+    key: 'manName',
+    initialLabel: 'Enter a man\'s name',
+    finalLabel: null,
+    value: 'Dave',
+  },
+  {
+    key: 'womanName',
+    initialLabel: 'Enter a woman\'s name',
+    finalLabel: 'Met',
+    value: 'Julie',
+  },
+  {
+    key: 'location',
+    initialLabel: 'At',
+    finalLabel: 'At',
+    value: 'Houses of Parliament',
+  },
+  {
+    key: 'manSaid',
+    initialLabel: 'He Said',
+    finalLabel: 'He Said',
+    value: 'Why the long face?',
+  },
+  {
+    key: 'womanSaid',
+    initialLabel: 'She Said',
+    finalLabel: 'She Said',
+    value: 'Where\'s wally?',
+  },
+  {
+    key: 'andThen',
+    initialLabel: 'And Then',
+    finalLabel: 'And Then',
+    value: 'They lived happily ever after',
+  },
+];
+/* eslint-enable max-len */
