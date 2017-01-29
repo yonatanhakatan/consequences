32d7d4c4142ab34097cd29a8a9dabfd4b28e98cf
Jonny Levy <iamjonnylevy@gmail.com>
2017-01-29 18:10:11 +0000

    test(welcome): add spec
    Signed-off-by: Jonny Levy <iamjonnylevy@gmail.com>

--- assets/react/components/welcome/Welcome.spec.js
+++ assets/react/components/welcome/Welcome.spec.js
@@ -5,6 +5,11 @@ import Welcome from './Welcome';
 import { CURRENT_GAMES } from '../../mocks/currentGames';
 
 describe('Welcome', () => {
+  it('shows the "Login with Facebook" button when the user is not authenticated', () => {
+    const user = { authenticated: false };
+    const element = shallow(<Welcome user={user} />);
+    expect(element.find('button.fbLogin').length).toEqual(1);
+  });
   it('shows the correct number of games that the authenticated user is involved in', () => {
     const user = { authenticated: true };
     const element = shallow(<Welcome currentGames={CURRENT_GAMES} user={user} />);
